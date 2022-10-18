83cc36f0154ec50daef7399c3108cb3eb028ecfe
ChessbeatsCheckers <86694290+ChessbeatsCheckers@users.noreply.github.com>
2022-10-18 12:51:14 -0700

    boardgeneration for p1 complete

--- Battleship3Dexperiment/Assets/scripts/createboard.cs
+++ Battleship3Dexperiment/Assets/scripts/createboard.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class createboard : MonoBehaviour
@@ -10,14 +11,14 @@ public class createboard : MonoBehaviour
     private GameObject[,] p2board = new GameObject[10, 10];    // Start is called before the first frame update
     void Start()
     {
-        int row = 1; 
-        int col = 1;
-
-       GameObject ob = GameObject.Instantiate(boardpiece);
-       p1board[row, col] = GameObject.Instantiate(boardpiece);
-       p1board[row, col].transform.position = new Vector3(col, boardpiece.transform.position.y, row);
+        //int row = 1; 
+       // int col = 1;
+//
+      // GameObject ob = GameObject.Instantiate(boardpiece);
+      // p1board[row, col] = GameObject.Instantiate(boardpiece);
+      // p1board[row, col].transform.position = new Vector3(col, boardpiece.transform.position.y, row);
        
-       //createboards(boardpiece);
+       createboards(boardpiece);
     }
 
     // Update is called once per frame
@@ -30,25 +31,58 @@ public class createboard : MonoBehaviour
     // up is z right is x
       void createboards(GameObject boardpiece)
       {
-        int row = 0;
-        int col = 0;
+        char colc = 'A';
+        
 
-        while (col < 10)
+        for (int col = 0; col < 10; col++)
+        {
+            for (int row = 0; row < 10; row++)
+            {
 
-            
-            while (row < 10)
-            { 
+
                 
+                Debug.Log("test");
                 p1board[row,col] = GameObject.Instantiate(boardpiece);
                 p1board[row, col].transform.position = new Vector3(col, boardpiece.transform.position.y, row);
-                row++;
+                string name = "B1[" + col + ":" + row + "]";
+                p1board[row, col].transform.name = "BoardPiece" ;
+                p1board[row, col].transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = name;
             }
-            row = 0;
-            col++;
+
         }
 
+
+
+        boardpiece.transform.position = new Vector3(1000, 1000, 1000);
+        }
+
+    char decideletter(int col)
+    {
+        if (col == 1)
+            return 'B';
+        else if (col == 2)
+            return 'C';
+        else if (col == 3)
+            return 'D';
+        else if (col == 4)
+            return 'E';
+        else if (col == 5)
+            return 'F';
+        else if (col == 6)
+            return 'G';
+        else if (col == 7)
+            return 'H';
+        else if (col == 8)
+            return 'I';
+        else if (col == 9)
+            return 'L';
+        else
+            return 'A';
+
+
+    }
         
-       }
+ }
 
 
       
