59d805d9ee9e95e31b272031a1baa590c4d590e5
ChessbeatsCheckers <86694290+ChessbeatsCheckers@users.noreply.github.com>
2022-10-18 11:43:40 -0700

    up

--- Battleship3Dexperiment/Assets/scripts/createboard.cs
+++ Battleship3Dexperiment/Assets/scripts/createboard.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 public class createboard : MonoBehaviour
 {
-    public GameObject boardpiece; 
+    public GameObject boardpiece;
     private GameObject[,] p1board = new GameObject[10,10];
-    private GameObject[,] p2board = new GameObject[10, 10]; 
-    // Start is called before the first frame update
+    private GameObject[,] p2board = new GameObject[10, 10];    // Start is called before the first frame update
     void Start()
     {
-        createboards(boardpiece);
+        int row = 1; 
+        int col = 1;
+
+       GameObject ob = GameObject.Instantiate(boardpiece);
+       p1board[row, col] = GameObject.Instantiate(boardpiece);
+       p1board[row, col].transform.position = new Vector3(col, boardpiece.transform.position.y, row);
+       
+       //createboards(boardpiece);
     }
 
     // Update is called once per frame
