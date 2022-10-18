7995da59cf5d0a1bae5a1584a515512692617313
ChessbeatsCheckers <86694290+ChessbeatsCheckers@users.noreply.github.com>
2022-10-18 10:40:23 -0700

    update

--- Battleship3Dexperiment/Assets/scripts/createboard.cs
+++ Battleship3Dexperiment/Assets/scripts/createboard.cs
@@ -11,7 +11,7 @@ public class createboard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        createboards(boardpiece);
     }
 
     // Update is called once per frame
@@ -21,26 +21,29 @@ public class createboard : MonoBehaviour
     }
 
 
-
+    // up is z right is x
       void createboards(GameObject boardpiece)
       {
         int row = 0;
         int col = 0;
 
         while (col < 10)
-        {
+
+            
             while (row < 10)
-            {
-                p1board[row,col] = GameObject.Instantiate(boardpiece);
+            { 
                 
+                p1board[row,col] = GameObject.Instantiate(boardpiece);
+                p1board[row, col].transform.position = new Vector3(col, boardpiece.transform.position.y, row);
                 row++;
             }
+            row = 0;
             col++;
         }
 
-        Debug.Log("asda"); 
+        
        }
 
 
       
-}
+
