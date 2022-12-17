49310d8ac6c846d70025296d24dcb9399ac644fc
ChessbeatsCheckers <86694290+ChessbeatsCheckers@users.noreply.github.com>
2022-12-17 13:20:08 -0800

    setupphase for hitting playerboats complete

--- Battleship3Dexperiment/Assets/scripts/endgame.cs
+++ Battleship3Dexperiment/Assets/scripts/endgame.cs
@@ -5,11 +5,14 @@ using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.TerrainTools;
+using UnityEngine.UIElements;
+using Random = System.Random;
 
 public class endgame:MonoBehaviour
 {
-    public int count = 0; 
-
+    private Random random = new Random();
+    public int count = 0;
+    private int turnsim=0;
     public GameObject Gendata;
     public GameObject score;
     public GameObject turn;
@@ -18,9 +21,10 @@ public class endgame:MonoBehaviour
     public Material blue;
     public Material white;
     public int[,] AiShipsplaced;
+    public int[,] plrshipsplaced = new int[10, 10];
     GameObject[,] p1board;
     GameObject[,] enemynpcboard;
-
+    Transform cameraogposition; 
     // Start is called before the first frame update
 
     public endgame(GameObject[,] p1board, GameObject[,] npcboard, GameObject Gendataui, GameObject scoreui, GameObject turnui, GameObject camera, GameObject campos, Material blue)
@@ -47,7 +51,7 @@ public class endgame:MonoBehaviour
         /// data setting above
         movecamerafriendlyboard();
         enableboard2();
-
+        numericalizeplrarre();
 
 
 
@@ -56,6 +60,7 @@ public class endgame:MonoBehaviour
 
     void movecamerafriendlyboard()
     {
+        cameraogposition = camera.transform ;
         camera.transform.position = friendlycampos.transform.position;
     }
 
@@ -105,7 +110,7 @@ public class endgame:MonoBehaviour
     }
     
     //-1 a hit spot, 0 water, -2 a hit ship 
-    int hitboard( int cordx, int cordy, int [,] board, string boardname)
+    int hitboard( int cordx, int cordy, string boardname)
     {
 
         if (boardname == "ai")
@@ -122,17 +127,17 @@ public class endgame:MonoBehaviour
             {
              return 0;
             }
-                return 1;
+            return 1;
         }///ai boardm hit ends
         else 
-        { 
-        
+        {
+            return 1;
         }
     }
 
     void GetGameObjectandmakeotherswhite(string rowcol, Transform obj)
     {
-        int hitsuccess;
+        int hitsuccess=-100;
         int rowe = rowcol[0] - '0';
         //change on turn
         int cole = rowcol[3] - '0';
@@ -153,21 +158,55 @@ public class endgame:MonoBehaviour
                     cleanboard();
                     if (enemynpcboard[row, col].transform.GetComponent<Renderer>().material.color != Color.green)
                         enemynpcboard[row, col].transform.GetComponent<Renderer>().material = blue;
-                    paintaiboard();
+
+                    paintboards();
+
+                    if (!OddOrEven(turnsim))// if odd ai hits plrboard
+                    {
+                        rowe= random.Next(1, 11);
+                        cole= random.Next(1, 11);
+
+                        hitsuccess = hitboard(rowe, cole, "plr");
+                        if (hitsuccess == 1)
+                        {
+                            camera.transform.position = cameraogposition.position;
+                            turnsim++;
+
+                            // next turn
+                        }
+                        {
+
+                            //error message
+                        }
+                    }
+                    //// AI HITTING PLAYER ABOVE AND PLAYER HITTING AI BELOW-----------------------------
                     if (Input.GetMouseButtonUp(0))
                     {
-                        hitsuccess = hitaiboard(rowe, cole);
+                        if (OddOrEven(turnsim))
+                        {
+                            hitsuccess = hitboard(rowe, cole, "ai");
+
+                        }
+                        
+
                         if (hitsuccess == 1)
-                        { 
+                        {
+                            camera.transform.position = cameraogposition.position;
+                            turnsim++;
+                            
                          // next turn
                         }
                         { 
+
                           //error message
                         }
 
                         // shooting missle
                         printarre(AiShipsplaced);
                     }
+
+
+
                 }
             }
 
@@ -180,16 +219,33 @@ public class endgame:MonoBehaviour
 
     }
 
-    void paintaiboard() /// work on this
+    void paintboards() /// work on this
     {
         for (int col2 = 0; col2 < 10; col2++)
         {
             for (int row2 = 0; row2 < 10; row2++)
             {
                 if (AiShipsplaced[col2, row2] == -1 || AiShipsplaced[col2, row2] == -2) {
-                    enemynpcboard[row2, col2].transform.GetComponent<Renderer>().material.color = Color.red;
+                    if(AiShipsplaced[col2, row2] == -1 )
+                    enemynpcboard[row2, col2].transform.GetComponent<Renderer>().material.color = Color.grey;
+
+                    if (AiShipsplaced[col2, row2] == -2)
+                        enemynpcboard[row2, col2].transform.GetComponent<Renderer>().material.color = Color.red;
+
                 }
-               // enemynpcboard[row2, col2].transform.GetComponent<BoxCollider>().enabled = true;
+
+                if (plrshipsplaced[col2, row2] == -1 || plrshipsplaced[col2, row2] == -2)
+                {
+                    if (plrshipsplaced[col2, row2] == -1)
+                        p1board[row2, col2].transform.GetComponent<Renderer>().material.color = Color.grey;
+
+                    if (plrshipsplaced[col2, row2] == -2)
+                        p1board[row2, col2].transform.GetComponent<Renderer>().material.color = Color.red;
+
+                }
+
+
+                // enemynpcboard[row2, col2].transform.GetComponent<BoxCollider>().enabled = true;
             }
         }
     }
@@ -232,8 +288,35 @@ public class endgame:MonoBehaviour
         for (int col = 0; col < 10; col++)
         {
 
-            Debug.Log(AiShipsplaced[0, col] + " " + AiShipsplaced[1, col] + " " + AiShipsplaced[2, col] + " " + AiShipsplaced[3, col] + " " + AiShipsplaced[4, col] + " " + AiShipsplaced[5, col] + " " + AiShipsplaced[6, col] + " " + AiShipsplaced[7, col] + " " + AiShipsplaced[8, col] + " " + AiShipsplaced[9, col] + " ");
+            Debug.Log(a[0, col] + " " + a[1, col] + " " + a[2, col] + " " + a[3, col] + " " + a[4, col] + " " + a[5, col] + " " + a[6, col] + " " + a[7, col] + " " + a[8, col] + " " + a[9, col] + " ");
         }
+    }
+      bool OddOrEven(int num)
+    {
+        return num % 2 == 0;
+    }
+
+
+
+    void numericalizeplrarre()
+    {
+
+     
+
+        for (int col2 = 0; col2 < 10; col2++)
+        {
+            for (int row2 = 0; row2 < 10; row2++)
+            {
+                if (p1board[col2, row2].GetComponent<Renderer>().material.color == Color.green)
+                    plrshipsplaced[col2, row2] = 1;
+                else
+                    plrshipsplaced[col2, row2] = 0;
+
+            }
+        }
+
+        Debug.Log("this is the plrs board-=-------------------------------------------------=================================================------------------================-----");
+        printarre(plrshipsplaced);
     }
 
 }
